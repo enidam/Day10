17e285edb95693b7f8635f7198c1e4dad30bcbb2
enida <enida_mujezin_16yahoo.com>
2021-03-18 09:24:15 +0100

    ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function myFunction(myObj, checkProp){
+var movie = {
+    title: "Titanic",
+    song: "My Heart Will Go On",
+    genere: "Drama"
+};
+movie.hasOwnProperty();
+
+return "Not Found"
+};
+console.log(myFunction("title"))
+module.exports= myFunction;
